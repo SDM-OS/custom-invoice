0f025c27c21aa426baa5ee92816cf186c7054398
Shashi Essp <shashiessp@gmail.com>
2015-06-02 23:27:50 +0530

    added new models

---
+++ main/migrations/0006_auto_20150602_1757.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('main', '0005_auto_20150602_1743'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='job',
+            name='end_date',
+            field=models.DateTimeField(default=None, blank=True),
+        ),
+        migrations.AddField(
+            model_name='job',
+            name='start_date',
+            field=models.DateTimeField(default=None, blank=True),
+        ),
+    ]
